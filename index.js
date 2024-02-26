e8eceb79be73087a4d0dbb3d0215e6b175cc8060
Sebastian Oliveto <sebastian.oliveto@hotmail.com>
2024-02-26 10:15:03 -0300

    cors all

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 //['https://example-agencia-de-viajes.netlify.app/',
 
-app.use(cors({
+app.use(cors());
+/*{
   origin:['https://example-agencia-de-viajes.netlify.app','http://localhost:5173','http://localhost:3001'],credentials:true}));
-
+*/
 app.use(router);
 // manejo de errores
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Error interno del servidor', error: err.stack });
-  });
+});
   
 
 app.listen(PORT,()=>{
